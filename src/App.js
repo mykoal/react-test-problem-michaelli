4fab22a6db502ffacd382af9566c06d9cfc9244d
Michael <113119817+mykoal@users.noreply.github.com>
2023-02-10 03:37:37 -0500

    Tasks done:
    1. add a button to the default page in App.js, labeled "click me"
    2. add a handler that changes the text in the button to "thanks" when clicked once, and back to "click me" if clicked again

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      buttonText: 'click me'
     };
   }
 
   //handler goes here
-
+  handleButtonClick = () => {
+    this.setState(prevState => ({
+      buttonText: prevState.buttonText === 'click me' ? 'thanks' : 'click me'
+    }));
+  };
+  
   render() {
     return (
       <div>
         <p>Greetings!</p>
-        <p>Button goes here</p>
+        <button onClick={this.handleButtonClick}>{this.state.buttonText}</button>
       </div>
     );
   }
