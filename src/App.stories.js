593962683d2c82329bf0dda6ab97967ed24079b2
Michael <113119817+mykoal@users.noreply.github.com>
2023-02-11 14:20:21 -0500

    Update App.stories.js

--- src/App.stories.js
+++ src/App.stories.js
@@ -1,2 +1,10 @@
 // Stories for testing `App` go here
 // See https://storybook.js.org/docs/react/writing-stories/introduction#how-to-write-stories
+
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import App from './App';
+
+storiesOf('App', module)
+  .add('default', () => <App />);
+  
